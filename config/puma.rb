741848623d30997f51332da778a1d3463eee1d6e
Saving Whalesharks <savethewhalesharks3@gmail.com>
2017-02-18 17:29:59 +0000

    Commented out cinfig/puma.rb original statements with ##

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,22 @@
+##    These were commented out commands 2/18/17 in order to try and get the Puma
+##    server working for production on heroku
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+##    threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+##    threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+##    port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+##    environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -44,7 +47,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # end
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+##    plugin :tmp_restart
 
 workers Integer(ENV[ 'WEB_CONCURRENCY' ] || 2)
 threads_count = Integer(ENV[ 'RAILS_MAX_THREADS' ] || 5)
